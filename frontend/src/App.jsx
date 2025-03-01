600dae99bb53bfaf79f55cad4195cbe67a4abe63
Sharans23 <sharan.shetty2309@gmail.com>
2025-03-01 13:26:32 +0530

    Dashboard page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"; 
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
 import { ThemeProvider } from "@emotion/react";
 import fontcolorTheme from "./fontColorTheme";
 import TeacherDashboard from "./TeacherDashboard";
@@ -12,43 +12,51 @@ import SignUpStudent from "./SignUpStudent";
 import Home from "./Home";
 import StudentDashboard from "./StudentDashboard";
 import AddMaterial from "./AddMaterial";
-import Quiz from "./Quiz"
-import { EmailProvider } from './EmailContext';
+import Quiz from "./Quiz";
+import { EmailProvider } from "./EmailContext";
 import { UsernameProvider } from "./UsernameContext";
 import OtherComponent from "./OtherComponent";
 import ScopeReco from "./ScopeRec";
 import Recommendations from "./MaterialRec";
 import Resources from "./Resources";
-
+import Dashboard from "./Sashboard";
 
 function App() {
-
- 
   return (
     <div className="App">
       <div>
-       <ThemeProvider theme={fontcolorTheme}>
-        <UsernameProvider>
-       <EmailProvider>
-         <Router> 
-          <Routes>
-            <Route path="/scoperecommendations" element={<ScopeReco />} /> 
-            <Route path="/materialrecommendations" element={<Recommendations/>} /> 
-            <Route path="/teacherDashboard" element={<TeacherDashboard />} />
-            <Route path="/studentDashboard" element={<StudentDashboard />} />
-            <Route path="/addstudents" element={<AddStudents />} />
-            <Route path="/addmaterial" element={<AddMaterial/>} />
-            <Route path="/quiz" element={<Quiz/>} />
-            <Route path="/resources" element={<Resources />} />
-            <Route path="/loginTeacher" element={<LoginTeacher/>} />
-            <Route path="/signupTeacher" element={<SignUpTeacher/>} />
-            <Route path="/loginStudent" element={<LoginStudent/>} />
-            <Route path="/signupStudent" element={<SignUpStudent/>} />
-            <Route path="/" element={<Home />} />
-          </Routes>
-        </Router>
-        </EmailProvider>
-        </UsernameProvider>
+        <ThemeProvider theme={fontcolorTheme}>
+          <UsernameProvider>
+            <EmailProvider>
+              <Router>
+                <Routes>
+                  <Route path="/scoperecommendations" element={<ScopeReco />} />
+                  <Route
+                    path="/materialrecommendations"
+                    element={<Recommendations />}
+                  />
+                  <Route
+                    path="/teacherDashboard"
+                    element={<TeacherDashboard />}
+                  />
+                  <Route
+                    path="/studentDashboard"
+                    element={<StudentDashboard />}
+                  />
+                  <Route path="/addstudents" element={<AddStudents />} />
+                  <Route path="/addmaterial" element={<AddMaterial />} />
+                  <Route path="/quiz" element={<Quiz />} />
+                  <Route path="/resources" element={<Resources />} />
+                  <Route path="/loginTeacher" element={<LoginTeacher />} />
+                  <Route path="/signupTeacher" element={<SignUpTeacher />} />
+                  <Route path="/loginStudent" element={<LoginStudent />} />
+                  <Route path="/signupStudent" element={<SignUpStudent />} />
+                  <Route path="/student-dashboard" element={<Dashboard />} />
+                  <Route path="/" element={<Home />} />
+                </Routes>
+              </Router>
+            </EmailProvider>
+          </UsernameProvider>
         </ThemeProvider>
       </div>
     </div>
