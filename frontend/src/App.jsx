0780bce1edfb6fef88ee31e91add13dd4b5fb6a8
Sharans23 <sharan.shetty2309@gmail.com>
2025-03-01 10:21:14 +0530

    Initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Button } from "@mui/material";
+
+const App = () => {
+  return (
+    <div>
+      <Button>Hello</Button>
+      <div className="text-5xl text-red-800 bg-amber-700 p-6">App</div>
+    </div>
+  );
+};
+
+export default App;
